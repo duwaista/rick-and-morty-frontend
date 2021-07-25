d4c3fb001992892012004983a66ed0088e37ca37
Vladislav Egorov <vlad.45.45@mail.ru>
2021-07-25 22:24:39 +0700

    Add sorts

--- src/components/EpisodeList/EpisodeList.tsx
+++ src/components/EpisodeList/EpisodeList.tsx
@@ -5,36 +5,69 @@ import EpisodeItem from "../EpisodeItem/EpisodeItem";
 
 export default function EpisodeList() {
   const allEpisodesUrl = "https://rickandmortyapi.com/api/episode";
+  const [episodesInfo, setEpisodeInfo] = useState([]);
   const [results, setResults] = useState([]);
 
   useEffect(() => {
-		// Запрос к серверу
-    axios.get(allEpisodesUrl)
+    // Запрос к серверу
+    axios
+      .get(allEpisodesUrl)
       .then((response) => {
         setResults(response.data.results);
+        setEpisodeInfo(response.data.info);
+        console.log(response.data.info);
       })
       .catch((error) => {
         console.error(error);
       });
   }, []);
 
-	const nameSort = () => {
-		// console.log(Object.getOwnPropertyNames(results));
-		console.log(results)
-	}
+  const resultsSort = (sortType: string) => {
+    // Сохраняем значение, чтобы менять состояние
+    const sorteredResults = [...results];
+
+    if (sortType === "name") {
+      // Сортируем массив по названию серии
+      sorteredResults.sort((a, b) => a.name.localeCompare(b.name));
+      setResults(sorteredResults);
+    } else {
+      // Сортируем массив по дате (сервер гарантирует, что id соответсвует дате выхода)
+      sorteredResults.sort((a, b) => a.id - b.id);
+      setResults(sorteredResults);
+    }
+  };
 
   return (
     <>
       <div className="episode-list-container">
         <div className="episodes-list-item-titles">
-          <span className="episode-title-number">#</span>
-          <span onClick={nameSort} className="episode-title-name">Название</span>
-          <span className="episode-title-date">Дата выхода</span>
+          <span
+            onClick={() => resultsSort("id")}
+            title="Сортировать по серии"
+            className="episode-title-number"
+          >
+            #
+          </span>
+          <span
+            onClick={() => resultsSort("name")}
+            title="Сортировать по имени"
+            className="episode-title-name"
+          >
+            Название
+          </span>
+          <span
+            onClick={() => resultsSort("date")}
+            title="Сортировать по дате"
+            className="episode-title-date"
+          >
+            Дата выхода
+          </span>
         </div>
-        {/* Без мемоизации, т.к. используется внутреннее состояние компонента */}
-        {results.map((result, index) => (
-          <EpisodeItem key={result.id} props={result} />
-        ))}
+        {useMemo(() => {
+          return results.map((result, index) => (
+            <EpisodeItem key={index} props={result} />
+          ));
+        }, [results])}
       </div>
     </>
   );
